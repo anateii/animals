53b552124d8ea58ec2b80ded003161cacc56b098
Ana Matei <anamatei.099@gmail.com>
2023-01-19 00:46:56 +0100

    Adding final css styling

--- src/AnimalShow.js
+++ src/AnimalShow.js
@@ -1,4 +1,5 @@
-import React from "react";
+import "./AnimalShow.css";
+import React, { useState } from "react";
 import bird from "./svg/bird.svg";
 import cat from "./svg/cat.svg";
 import horse from "./svg/horse.svg";
@@ -17,9 +18,21 @@ const svgMap = {
 };
 
 const AnimalShow = ({ type }) => {
+  const [clicks, setClicks] = useState(0);
+
+  const handleClick = () => {
+    setClicks(clicks + 1);
+  };
+
   return (
-    <div>
-      <img alt="animal" src={svgMap[type]} />
+    <div className="animal-show" onClick={handleClick}>
+      <img className="animal" alt="animal" src={svgMap[type]} />
+      <img
+        className="heart"
+        alt="heart"
+        src={heart}
+        style={{ width: 10 + 10 * clicks + "px" }}
+      />
     </div>
   );
 };
